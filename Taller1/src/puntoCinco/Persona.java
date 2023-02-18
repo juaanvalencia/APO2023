182b6c037d8ab103346c9cc71f4d0c8cd0b08c7f
juaanvalencia <125085415+juaanvalencia@users.noreply.github.com>
2023-02-17 19:24:45 -0500

    Add files via upload

---
+++ Taller1/src/puntoCinco/Persona.java
@@ -0,0 +1,18 @@
+package puntoCinco;
+
+import java.util.Scanner;
+
+public class Persona {
+	public Persona(String name, int edad) {
+		
+	}
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner ( System.in);
+		System.out.println ("ingrese el nombre de la persona");
+		String name = sc.next();
+		
+	}
+}
