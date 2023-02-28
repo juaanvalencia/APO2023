222ab9fdceec09a2c083cee395184ccf38f660d4
juaanvalencia <125085415+juaanvalencia@users.noreply.github.com>
2023-02-28 15:57:46 -0500

    Taller3

---
+++ TallerN3/src/taller/Punto1.java
@@ -0,0 +1,23 @@
+package taller;
+
+public class Punto1 {
+
+		// Aquí hay tres formas diferentes de declarar e instanciar un objeto de cadena llamado "myString" que contenga "abc":
+		
+		// 1 - Utilizando la palabra clave "var":
+		
+		 var myString = "abc";
+		 
+	    // 2 - Utilizando la palabra clave "let":
+		 
+		 let myString = "abc";
+		 
+		// 3 - Utilizando la palabra clave "const":
+		 
+		 const myString = "abc";
+
+
+		
+	}
+
+}
