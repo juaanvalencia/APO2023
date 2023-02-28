2c66768acca7e5b17139e967295824e3bc81d098
juaanvalencia <125085415+juaanvalencia@users.noreply.github.com>
2023-02-28 16:53:27 -0500

    Taller3

---
+++ TallerN3/src/taller/Punto3.java
@@ -0,0 +1,11 @@
+package taller;
+
+public class Punto3 {
+
+	public static void main (String[] args) {
+		String str1 = new String ("AEI");
+		String str2 = new String ("OU");
+		String str3 = new String (str1 + str2);
+		System.out.print(str3);
+	}
+}
